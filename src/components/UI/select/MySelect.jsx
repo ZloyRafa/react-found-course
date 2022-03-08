ed9652ea2a8d186fd5789030fcb938d5f6842701
ZloyRafa <raf8143@gmail.com>
2022-03-09 02:55:02 +0300

    fow work

---
+++ src/components/UI/select/MySelect.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const MySelect = ({options, defaultValue, value, onChange}) => {
+    return (
+        <select 
+            value= {value}
+            onChange = {event => onChange(event.target.value)}
+        >
+            <option disabled value="">{defaultValue}</option>
+            {options.map(option =>
+                <option key={option.value} value={option.value}>
+                    {option.title}
+                </option>
+            )}
+        </select>
+    );
+};
+ export default MySelect;
